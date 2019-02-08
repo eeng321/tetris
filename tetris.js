f172ef210295ff5b29b935d99766711a16de9638
edmond <edmond.e321@gmail.com>
2019-02-08 02:45:12 -0800

    implement drop

--- tetris.js
+++ tetris.js
@@ -6,53 +6,54 @@ canvas.width = GRID_WIDTH;
 canvas.height = GRID_HEIGHT;
 
 var g = canvas.getContext("2d");
-var gNext = canvas.getContext("2d");
 
 var EMPTY = -1; //empty square
 var COLS = 10; //number of columns
 var ROWS = 20; //number of rows
+var BUFFER = 10; //invisible grid
 var SQUARE_SIZE = 40;
 var grid = []; //grid array
 var speed; //piece fall speed
 var active; //active piece
 var next; //next active piece
+var spawn = { r: 0, c: 5 }; //row,col
 
 //row, col
 var OPiece = [
-    [[1, -1], [1, 0], [2, -1], [2, 0]]
+    [[0, -1], [0, 0], [1, -1], [1, 0]]
 ]
 
 var IPiece = [
-    [[1, -2], [1, -1], [1, 0], [1, 1]],
+    [[0, -2], [0, -1], [0, 0], [0, 1]],
     [[0, 0], [1, 0], [2, 0], [3, 0]]
 ]
 
 var SPiece = [
-    [[1, 0], [1, 1], [2, 0], [2, -1]],
+    [[0, 0], [0, 1], [1, 0], [1, -1]],
     [[1, 0], [0, 0], [1, 1], [2, 1]]
 ]
 
 var ZPiece = [
-    [[1, 0], [1, -1], [2, 0], [2, 1]],
+    [[0, 0], [0, -1], [1, 0], [1, 1]],
     [[1, 0], [0, 1], [2, 0], [1, 1]]
 ]
 
 var LPiece = [
-    [[1, 0], [1, -1], [1, 1], [2, -1]],
+    [[0, 0], [0, -1], [0, 1], [1, -1]],
     [[1, 0], [0, 0], [2, 0], [2, 1]],
     [[1, 0], [1, -1], [1, 1], [0, 1]],
     [[1, 0], [2, 0], [0, -1], [0, 0]]
 ]
 
 var JPiece = [
-    [[1, 0], [1, -1], [1, 1], [2, 1]],
+    [[0, 0], [0, -1], [0, 1], [1, 1]],
     [[1, 0], [2, 0], [0, 0], [0, 1]],
     [[1, 0], [0, -1], [1, -1], [1, 1]],
     [[1, 0], [2, -1], [2, 0], [0, 0]]
 ]
 
 var TPiece = [
-    [[1, 0], [1, -1], [1, 1], [2, 0]],
+    [[0, 0], [0, -1], [0, 1], [1, 0]],
     [[1, 0], [0, 0], [1, 1], [2, 0]],
     [[1, 0], [1, -1], [1, 1], [0, 0]],
     [[1, 0], [0, 0], [1, -1], [2, 0]]
@@ -66,10 +67,11 @@ var colors = ["#ffff00", "#00bfff", "#80ff00", "#ff4000", "#ffbf00", "#0000ff", 
 
 
 class Piece {
-    constructor(piece, o, color) {
+    constructor(piece, o, location, color) {
         this.type = piece;
-        this.orientation = o;
-        this.color = color;
+        this.orientation = o; //int
+        this.location = location;
+        this.color = color; //int
         this.isFalling = true;
         this.isActive = true;
     }
@@ -79,19 +81,22 @@ function spawnRandomPiece() {
     var rand = Math.floor(Math.random() * (pieces.length));
     var type = pieces[rand]; //piece type
     var o = Math.floor(Math.random() * type.length); //orientation
-    var rp = new Piece(type, o, rand); //random piece
+    var p = type[o];
+    var location = [];
+    for (var i = 0; i < p.length; i++) {
+        location[i] = new Array(2);
+        location[i][0] = p[i][0] + spawn.r; //compute row location
+        location[i][1] = p[i][1] + spawn.c; //compute col location
+    }
+    var rp = new Piece(type, o, location, rand); //random piece
     active = rp;
     //var col = Math.floor(Math.random() * COLS);
-    var spawn = [0, 5]; //row,col
-    drawPiece(rp, spawn); //spawn base orientation of a random piece at start location
+    drawPiece(rp); //spawn base orientation of a random piece at start location
 }
 
-function drawPiece(piece, location) {
-    var p = piece.type[piece.orientation];
-    for (var i = 0; i < p.length; i++) {
-        p[i][0] += location[0]; //compute row location
-        p[i][1] += location[1]; //compute col location
-        mapSquare(p[i], piece.color);
+function drawPiece(piece) {
+    for (var i = 0; i < piece.type[piece.orientation].length; i++) {
+        mapSquare(piece.location[i], piece.color);
     }
 }
 
@@ -104,42 +109,64 @@ function drawBoardState() {
     for (var r = 0; r < ROWS; r++) {
         for (var c = 0; c < COLS; c++) {
             if (grid[r][c] >= 0) {
-                drawSquare(r,c);
+                drawSquare(r, c);
             }
         }
     }
 }
 
-function drawSquare(r, c) {
+function drawSquare(r, c, ) {
     g.fillStyle = colors[grid[r][c]];
     g.fillRect(c * SQUARE_SIZE, r * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
-    g.lineWidth = "1";
-    g.rect(c * SQUARE_SIZE, r * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
-    g.stroke();
+    g.strokeStyle = "white";
+    g.strokeRect(c * SQUARE_SIZE, r * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
 }
 
 
-function fallingPiece() {
-    console.log(active);
-    
+function drop() {
+    //temporary conditions
+    if (active.isActive && active.location[0][0] < 21) {
+        var r, c;
+        for (var i = 0; i < active.location.length; i++) {
+            r = active.location[i][0];
+            c = active.location[i][1];
+            removeSquare(r, c);
+            active.location[i][0] += 1;
+        }
+        for (var i = 0; i < active.location.length; i++) {
+            mapSquare(active.location[i], active.color);
+        }
+        drawBoardState();
+    }
+    else {
+        spawnRandomPiece();
+        drawBoardState();
+    }
+
 }
 
-function refresh(){
-    g = gNext;
-    g.stroke();
+function removeSquare(r, c) {
+    g.fillStyle = "black";
+    g.fillRect(c * SQUARE_SIZE, r * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
+    g.strokeStyle = "white";
+    g.strokeRect(c * SQUARE_SIZE, r * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
+    grid[r][c] = -1;
+}
+
+function collision(){
+
 }
 
 function drawGrid(r, c) {
     g.beginPath();
     g.lineWidth = "1";
     g.strokeStyle = "white"
-    g.rect(c * SQUARE_SIZE, r * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
-    g.stroke();
+    g.strokeRect(c * SQUARE_SIZE, r * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
 }
 
 function initGrid() {
     //init grid array
-    for (var row = 0; row < ROWS; row++) {
+    for (var row = 0; row < ROWS + BUFFER; row++) {
         grid[row] = new Array(COLS);
     }
 
@@ -152,14 +179,17 @@ function initGrid() {
 }
 
 function play() {
+    g.clearRect(0, 0, canvas.width, canvas.height);
+
+    initGrid();
+
     spawnRandomPiece();
-    if(active.isFalling && active.isActive){
-        setInterval(fallingPiece(), 500);
-    }
+    drawBoardState();
+
+    setInterval(drop,100);
 }
 
 function setup() {
-    initGrid();
     play();
-    drawBoardState();
 }
+
